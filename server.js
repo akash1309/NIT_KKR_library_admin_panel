c76ff5d6fd49fc484005722f737e1e1ec41e8c42
akash1309 <akash.goyal1309@gmail.com>
2018-10-19 13:21:38 +0530

    server.js created

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var http = require('http');
+var mysql   = require('mysql');
+var express = require('express');
+var app     = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "goyalanshu",
+  database: "NIT_KKR"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.listen(8080);
